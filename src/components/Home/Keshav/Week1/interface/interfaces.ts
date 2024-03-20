d88ccd6d6a02fac2c78d5e17e828899f2a5af93a
Ritika Garg <ritika.garg@intimetec.com>
2024-03-20 11:25:08 +0530

    Implementation of husky in the Joint Project

--- src/components/Home/Keshav/Week1/interface/interfaces.ts
+++ src/components/Home/Keshav/Week1/interface/interfaces.ts
@@ -17,7 +17,7 @@ export interface ToDoContextType {
   tasks: Task[];
   filterValue: string;
   addTask: (task: Task) => void;
-  updateTaskStatus: (taskId: number, value: any) => void;
+  updateTaskStatus: (taskId: number, value: string) => void;
   updateDueDate: (taskId: number, dueDate: string) => void;
   updateTaskDescription: (taskId: number, description: string) => void;
   setTasks: Dispatch<SetStateAction<Task[]>>;
